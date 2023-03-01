e36b148c8c553b1ca1f4b79d90f48ef388c95e7c
Mustafaelhdad <Mostafa.eng_2194@eng.kfs.edu.eg>
2023-03-01 16:34:51 +0200

    navigation menu(activate menu options in the wp dashboard in the apparence section) - active class for navigation elements(didn't work)

--- functions.php
+++ functions.php
@@ -13,6 +13,11 @@ function university_files() {
 add_action( 'wp_enqueue_scripts', 'university_files' );
 
 function university_features() {
+  // setup nav menus in wp dashboard
+  // register_nav_menu( 'headerMenuLocation', 'Header Menu Location' );
+  // register_nav_menu( 'footerLocationOne', 'Footer Location One' );
+  // register_nav_menu( 'footerLocationTwo', 'Footer Location Two' );
+  
   add_theme_support( 'title-tag' );
 }
 
